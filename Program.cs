9f14cd9b9266cf9dd92e5d9af4b6ab16dc9d1c55
higordutraa <higor.dutra@hotmail.com>
2024-09-03 21:21:52 -0300

    Questão 01-Vaga Estágio

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        // Defina o número a ser verificado
+        int numero = 21;
+
+        // Chamada da função para verificar se o número pertence à sequência de Fibonacci
+        bool pertence = PertenceSequenciaFibonacci(numero);
+
+        // Exibe a mensagem informando se o número pertence ou não à sequência
+        if (pertence)
+        {
+            Console.WriteLine($"O número {numero} pertence à sequência de Fibonacci.");
+        }
+        else
+        {
+            Console.WriteLine($"O número {numero} não pertence à sequência de Fibonacci.");
+        }
+    }
+
+    static bool PertenceSequenciaFibonacci(int num)
+    {
+        if (num < 0)
+            return false;
+
+        int a = 0;
+        int b = 1;
+
+        while (a < num)
+        {
+            int temp = a;
+            a = b;
+            b = temp + b;
+        }
+
+        return a == num;
+    }
+}
